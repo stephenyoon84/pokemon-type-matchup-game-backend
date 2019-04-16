ccd4dc4bb6d49f6355a4d08713cd1de81c91f95b
stephenyoon84 <stephenyoon84@gmail.com>
2019-04-15 22:57:39 -0400

    add seed data

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
   has_many :scores
   validates :name, :email, presence: true
-  validates :email, uniqness: true
+  # validates :email, uniqness: true
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-
+require 'open-uri'
 User.destroy_all
 Score.destroy_all
 Pokemon.destroy_all
@@ -13,3 +13,19 @@ Pokemon.destroy_all
 ann = User.create(name: "Ann", email: "ann.duong@example.com")
 ammar = User.create(name: "Ammar", email: "ammar.ali@example.com")
 stephen = User.create(name: "Stephen", email: "stephen.yoon@example.com")
+
+base_path = "https://pokeapi.co/api/v2/pokemon/"
+
+i = 1
+while i < 808 do
+  target = JSON.load(open("#{base_path}#{i}"))
+  # Pokemon.create(name: target["name"], type1: target["types"][0]["type"]["name"], type2: target["types"][1]["type"]["name"], image_url: target["sprites"]["front_default"])
+  # i += 1
+  if target["types"][1] != nil
+    Pokemon.create(name: target["name"], type1: target["types"][0]["type"]["name"], type2: target["types"][1]["type"]["name"], image_url: target["sprites"]["front_default"])
+    i += 1
+  else
+    Pokemon.create(name: target["name"], type1: target["types"][0]["type"]["name"], type2: nil, image_url: target["sprites"]["front_default"])
+    i += 1
+  end
+end
